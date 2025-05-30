c64b18fef69ed96c179fa553ad6805d4945e0514
Immow <k.schippers@gmail.com>
2025-05-30 17:23:16 +0200

    - multiline fix for text

--- libs/glorp/layouttests.lua
+++ libs/glorp/layouttests.lua
@@ -285,4 +285,18 @@ layoutTests[9] = Glorp.newContainer({
 	:addBlock({ w = 100, h = 100 })
 -- :addImage({ image = glorp_alien })
 
+layoutTests[10] = Glorp.newContainer({
+		titleBar = { enabled = true, cornerRadius = 10 },
+		id = "layoutTests",
+		x = 100,
+		y = 100,
+		enabled = false,
+		padding = 10
+	})
+	:addText({
+		text =
+		"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+		align = "left"
+	})
+
 return layoutTests
--- libs/glorp/text.lua
+++ libs/glorp/text.lua
@@ -2,18 +2,22 @@ local Text = {}
 Text.__index = Text
 
 function Text.new(settings)
-	local instance   = setmetatable({}, Text)
-	instance.id      = settings.id or nil
-	instance.type    = "text"
-	instance.text    = settings.text or settings.label or settings.id or ""
-	instance.font    = settings.font or love.graphics:getFont()
-	instance.color   = settings.color or { 1, 1, 1, 1 }
-	instance.align   = settings.align or "left" -- left, center, right
-	instance.x       = settings.x or 0
-	instance.y       = settings.y or 0
-	instance.w       = settings.w or math.min(instance.font:getWidth(instance.text), love.graphics.getWidth())
-	instance.h       = instance.font:getHeight()
-	instance.enabled = settings.enabled ~= false
+	local instance     = setmetatable({}, Text)
+	instance.id        = settings.id or nil
+	instance.type      = "text"
+	instance.text      = settings.text or settings.label or settings.id or ""
+	instance.font      = settings.font or love.graphics:getFont()
+	instance.color     = settings.color or { 1, 1, 1, 1 }
+	instance.align     = settings.align or "left" -- left, center, right
+	instance.x         = settings.x or 0
+	instance.y         = settings.y or 0
+	instance.w         = settings.w or math.min(instance.font:getWidth(instance.text), 400)
+
+	local lineHeight   = instance.font:getHeight()
+	local _, wrapCount = instance.font:getWrap(instance.text, instance.w)
+	instance.h         = #wrapCount * lineHeight
+
+	instance.enabled   = settings.enabled ~= false
 	return instance
 end
 
